dd5699e93c17dfdc4bdbac1f156456a7399f2c5c
Leonardo Sbarai <leonardo.sbarai@dextra-sw.com>
2019-08-21 16:42:40 -0300

    Drag and Drop, Select2

---
+++ ruby/capy/spec/drag_drop_spec.rb
@@ -0,0 +1,16 @@
+describe "Drag and Drop", :drop do
+  before(:each) do
+    visit "https://training-wheels-protocol.herokuapp.com/drag_and_drop"
+  end
+
+  it "homem aranha pertence ao time do stark" do
+    cap = find(".team-cap .column")         # início
+    stark = find(".team-stark .column")     # alvo
+    spiderman = find("img[alt$=Aranha]")    # img que quero enviar
+
+    spiderman.drag_to stark                 # ação de arrastar
+
+    expect(stark).to have_css "img[alt$=Aranha]" # confere se a imagem está no stark
+    expect(cap).not_to have_css "img[alt$=Aranha]" # confere se o container está vazio
+  end
+end
