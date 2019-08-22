19313ddef6447569fc2dad7d77c7ca96b090e710
Leonardo Sbarai <leonardo.sbarai@dextra-sw.com>
2019-08-22 15:36:04 -0300

    screenshot só quando gerar erro

--- ruby/capy/spec/spec_helper.rb
+++ ruby/capy/spec/spec_helper.rb
@@ -20,7 +20,7 @@ RSpec.configure do |config|
 
   config.after(:example) do |e|
     nome = e.description.gsub(/[^A-Za-z0-9 ]/, "").tr(" ", "_") # regex para substituir caracteres especiais por nada
-    page.save_screenshot("log/" + nome + ".png")
+    page.save_screenshot("log/" + nome + ".png") if e.exception # só tira print se der erro
   end
 end
 
