e82fd13ade2cf4a33ebbb7917b18ce436780d9c1
Leonardo Sbarai <leonardo.sbarai@dextra-sw.com>
2019-08-27 22:02:25 -0300

    Let

--- ruby/nflix-api/spec/post_spec.rb
+++ ruby/nflix-api/spec/post_spec.rb
@@ -1,61 +1,51 @@
 describe "post" do
   context "when new user" do
-    before do
-      @new_user = build(:user).to_hash # build a factory chamada :user já convertendo pra hash
-      @result = ApiUser.save(@new_user)
-    end
+    # before do
+    #   @new_user = build(:user).to_hash # build a factory chamada :user já convertendo pra hash
+    #   result = ApiUser.save(@new_user)
+    # end
 
-    it { expect(@result.response.code).to eql "200" }
+    # let pode substituir o método before
+    # let só é executado quando é chamado dentro da expectativa
+    # diferente do before que é sempre executado primeiro
+    let(:result) { ApiUser.save(build(:user).to_hash) }  # :result recebe o retorno do método ApiUser.save
+
+    it { expect(result.response.code).to eql "200" }
   end
 
   context "when duplicate email" do
-    before do
-      @new_user = build(:registered_user).to_hash
-      @result = ApiUser.save(@new_user)
-    end
+    let(:result) { ApiUser.save(build(:registered_user).to_hash) }
 
-    it { expect(@result.response.code).to eql "409" }
-    it { expect(@result.parsed_response["msg"]).to eql "Oops. Looks like you already have an account with this email adress." }
+    it { expect(result.response.code).to eql "409" }
+    it { expect(result.parsed_response["msg"]).to eql "Oops. Looks like you already have an account with this email adress." }
   end
 
   context "when wrong email" do
-    before do
-      @new_user = build(:user_wrong_email).to_hash
-      @result = ApiUser.save(@new_user)
-    end
+    let(:result) { ApiUser.save(build(:user_wrong_email).to_hash) }
 
-    it { expect(@result.response.code).to eql "412" }
+    it { expect(result.response.code).to eql "412" }
     # o parsed_response já pega a mensagem no formato hash de chave : valor para validar a msg de erro
-    it { expect(@result.parsed_response["msg"]).to eql "Oops. You entered a wrong email." }
+    it { expect(result.parsed_response["msg"]).to eql "Oops. You entered a wrong email." }
   end
 
   context "when empty name" do
-    before do
-      @new_user = build(:empty_name_user).to_hash
-      @result = ApiUser.save(@new_user)
-    end
+    let(:result) { ApiUser.save(build(:empty_name_user).to_hash) }
 
-    it { expect(@result.response.code).to eql "412" }
-    it { expect(@result.parsed_response["msg"]).to eql "Validation notEmpty on full_name failed" }
+    it { expect(result.response.code).to eql "412" }
+    it { expect(result.parsed_response["msg"]).to eql "Validation notEmpty on full_name failed" }
   end
 
   context "when empty email" do
-    before do
-      @new_user = build(:empty_email_user).to_hash
-      @result = ApiUser.save(@new_user)
-    end
+    let(:result) { ApiUser.save(build(:empty_email_user).to_hash) }
 
-    it { expect(@result.response.code).to eql "412" }
-    it { expect(@result.parsed_response["msg"]).to eql "Validation notEmpty on email failed" }
+    it { expect(result.response.code).to eql "412" }
+    it { expect(result.parsed_response["msg"]).to eql "Validation notEmpty on email failed" }
   end
 
   context "when empty password" do
-    before do
-      @new_user = build(:empty_password_user).to_hash
-      @result = ApiUser.save(@new_user)
-    end
+    let(:result) { ApiUser.save(build(:empty_password_user).to_hash) }
 
-    it { expect(@result.response.code).to eql "412" }
-    it { expect(@result.parsed_response["msg"]).to eql "Validation notEmpty on password failed" }
+    it { expect(result.response.code).to eql "412" }
+    it { expect(result.parsed_response["msg"]).to eql "Validation notEmpty on password failed" }
   end
 end
